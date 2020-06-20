e196ac17eb489abfda5eef8066d9de669a2300b6
Lazybotk <66409915+Lazybotk@users.noreply.github.com>
2020-06-20 10:02:57 +0530

    Update fun.py

--- tg_bot/modules/fun.py
+++ tg_bot/modules/fun.py
@@ -20,11 +20,11 @@ import nekos
 from telegram import Bot, Update, ParseMode, Message
 from telegram.ext import run_async
 
-import lynda.modules.fun_strings as fun_strings
-from lynda import dispatcher
-from lynda.modules.disable import DisableAbleCommandHandler
-from lynda.modules.helper_funcs.chat_status import is_user_admin
-from lynda.modules.helper_funcs.extraction import extract_user
+import tg_bot.modules.fun_strings as fun_strings
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin
+from tg_bot.modules.helper_funcs.extraction import extract_user
 
 import nltk
 from deeppyer import deepfry
