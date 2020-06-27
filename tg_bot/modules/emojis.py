3f16cd7cfe4bd4e37cfe8009fcf1df225436044a
ﾑズﾑﾘ <66409915+Lazybotk@users.noreply.github.com>
2020-06-27 08:12:56 +0530

    Update emojis.py

--- tg_bot/modules/emojis.py
+++ tg_bot/modules/emojis.py
@@ -133,7 +133,7 @@ def love(bot: Bot, update: Update):
     for x in range(EDIT_TIMES):
         msg.edit_text(love_siren[x%5])
         time.sleep(EDIT_SLEEP)
-    msg.edit_text('പ്രണയം  😂 ')
+    msg.edit_text('Love  😂 ')
 
 
 
